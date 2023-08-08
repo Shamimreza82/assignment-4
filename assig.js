06d5e2bcd964b100d770d96fad2fc2d665e6a7c0
Shamimreza82 <shamimrezaone@gmail.com>
2023-08-08 19:28:24 +0600

    update number

--- assig.js
+++ assig.js
@@ -1,1 +1,23 @@
-console.log( )
+
+// problem (Solved)
+
+function cubeNumber(number) {
+    if (typeof number !== "number"){
+        return "Please provide a number"
+    }
+
+    const cube = number; 
+    const power = 3; 
+    const output = Math.pow(cube, power)
+    return output; 
+
+
+}
+
+const numbers = "3"; 
+const cubeNumber1 = cubeNumber(numbers); 
+console.log(cubeNumber1);
+
+
+//--------------------------------------------------------------------------------------
+
